0029f8f078af142e3ea6c84ffd26619b016edb6e
Caio Vellani <caiovellani@outlook.com>
2024-11-06 21:50:37 -0300

    feat: Update project setup and add Header component
    - Update index.html:
      - Remove Vite favicon
      - Add Google Fonts preconnect and stylesheet links
      - Change page title to "Todo Desafio Rocketseat"
    - Modify App.tsx to include Header component
    - Import global CSS in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
+
 export function App() {
 
   return (
     <>
-      <div>Hello World</div>
+      <div>
+        <Header />
+      </div>
     </>
   )
 }

4399d20a0fb44a031fe5a2dffa50df86868fe164
Caio Vellani <caiovellani@outlook.com>
2024-11-06 22:09:52 -0300

    feat: Add main content structure and import new components
    - Import Button and Input components
    - Add CSS module for App component
    - Restructure App component with main and section elements
    - Include Input and Button components in the content section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import styles from './App.module.css';
+import { Button } from "./components/Button";
 import { Header } from "./components/Header";
-
+import { Input } from "./components/Input";
 export function App() {
-
+  
   return (
     <>
-      <div>
+      <main>
         <Header />
-      </div>
+
+        <section  className={styles.content}>
+          <div>
+            <Input></Input>
+            <Button></Button>
+          </div>
+        </section>
+        </main>
     </>
   )
 }
